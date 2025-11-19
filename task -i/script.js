6ef26f11aa3a32ba14b43089be5168669b4b825c
abdullahnur-centria <abdullah.nur@centria.fi>
2025-11-19 16:05:31 +0200

    Task I: Build a Rental Business Landing Page

--- task -i/script.js
+++ task -i/script.js
@@ -2,4 +2,6 @@ function viewRentals() {
   alert("Redirecting to rental options...");
   // In real implementation, you might do:
   // window.location.href = "/rentals.html";
+  // Author: Md Abdullah Ibne Shahid Nur
+  // Date: 2025-11-19
 }
