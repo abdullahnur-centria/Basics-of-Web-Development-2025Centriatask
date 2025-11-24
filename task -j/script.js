f1c73168ebb8a5137dd15f998194ebac55e56376
abdullahnur-centria <abdullah.nur@centria.fi>
2025-11-24 23:42:48 +0200

    Task J: Build a Rental Catalog Page

---
+++ task -j/script.js
@@ -0,0 +1,18 @@
+// Author: Md Abdullah Ibne Shahid Nur
+// Date: 2025-11-24
+
+// Function to handle the "See Available Rentals" button click
+
+
+function viewRentals() {
+  alert("Redirecting to rental options...");
+  // In production: window.location.href = "/catalog.html";
+}
+
+// Optional enhancement
+function scrollToExample() {
+  const exampleSection = document.querySelector(".example-section");
+  if (exampleSection) {
+    exampleSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
